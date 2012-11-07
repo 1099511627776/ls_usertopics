b6abf62ea72716e2d4ec110eb32dd46769fb0a4a
gigabyte <1099511627776@mail.ru>
2012-11-07 14:39:37 +0200

    removed debug messages

--- usertopics/classes/hooks/HookUsertopics.class.php
+++ usertopics/classes/hooks/HookUsertopics.class.php
@@ -14,7 +14,6 @@ class PluginUsertopics_HookUsertopics extends Hook {
 		if(Router::GetActionEventName()=='EventWhois') {
 			$sUserLogin = Router::GetActionEvent();
 			if (($oUserProfile=$this->User_GetUserByLogin($sUserLogin))) {
-				print_r($oUserPrifile);
 				$aResult=$this->Topic_GetTopicsPersonalByUser($oUserProfile->getId(),1,$iPage,Config::Get('module.topic.per_page'));
 				$aTopics=$aResult['collection'];
 				$this->Viewer_Assign('aTopics',$aTopics);
